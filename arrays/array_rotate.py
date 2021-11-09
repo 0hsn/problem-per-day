e028db718e658c6c294ac829717231a5736a57c6
HSN <hasanlock@gmail.com>
2021-11-09 22:58:01 +0600

    refactor: code style as pep8:e305
    - refactor: ignore ide files

--- arrays/array_rotate.py
+++ arrays/array_rotate.py
@@ -1,10 +1,12 @@
 def rotateArray(d, arr):
     return arr[d:] + arr[:d]
 
+
 if __name__ == '__main__':
     arr = [1, 4, 3, 2]
     arr_r = rotateArray(2, arr)
-    
+
     import pprint
+
     pprint.pprint(arr)
     pprint.pprint(arr_r)
